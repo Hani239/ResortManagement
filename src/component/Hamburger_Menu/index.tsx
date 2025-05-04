8fc612c3ced8436f71a13c65de7f8ecaf732ce2a
“Hani239” <hanivinaben2924@gmail.com>
2025-05-05 01:40:24 +0530

    Nav updated

--- src/component/Hamburger_Menu/index.tsx
+++ src/component/Hamburger_Menu/index.tsx
@@ -46,39 +46,10 @@ const HamburgerMenu = () => {
                 <div className="absolute right-0 w-screen mt-2  bg-white border ">
                     <div className="py-1">
                         <a href="/" className="block font-playpen text-xl px-4 py-2 border-b text-gray-800 ">Home</a>
-                        {/* <div className="block relative font-playpen text-xl px-4 py-2 pb-4 border-b text-gray-800 " onClick={toggleClothing}>Clothing
-                            <svg
-                                className={`w-6 h-6 absolute right-2 top-3 transition-transform ${isClothingOpen ? 'transform rotate-180' : ''
-                                    }`}
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    d={
-                                        isOpen
-                                            ? 'M19 9l-7 7-7-7'
-                                            : 'M5 15l7-7 7 7'
-                                    }
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                />
-                            </svg>
-                            <div>
-                                {isClothingOpen && (
-                                    <div className="bg-white w-screen">
-                                        <div className="py-1">
-                                        <Link href="/List" className="block font-playpen py-2 text-[1rem] text-gray-800">Newborn (0 m - 3 m)</Link>
-                                        <Link href="/List" className="block font-playpen py-2 text-[1rem] text-gray-800">Girls (3 m - 6 y)</Link>
-                                        <Link href="/List" className="block font-playpen py-2 text-[1rem] text-gray-800">Boys (3 m - 6 y)</Link>
-                                        </div>
-                                    </div>
-                                )}
-                            </div>
-                        </div> */}
                         <Link href="/List" className="block font-playpen text-xl px-4 py-2 border-b text-gray-800 ">Accomodation</Link>
-                        <Link href="/List" className="block font-playpen text-xl px-4 py-2 border-b text-gray-800 ">Events</Link>
-                        <a href="#" className="block font-playpen text-xl px-4 py-2 border-b text-gray-800 ">About</a>
-                        <a href="#" className="block font-playpen text-xl px-4 py-2 text-gray-800 ">Contact</a>
+                        <Link href="/Evnt" className="block font-playpen text-xl px-4 py-2 border-b text-gray-800 ">Events</Link>
+                        <a href="/About" className="block font-playpen text-xl px-4 py-2 border-b text-gray-800 ">About</a>
+                        <a href="/Contact" className="block font-playpen text-xl px-4 py-2 text-gray-800 ">Contact</a>
                     </div>
                 </div>
             )}
--- src/component/Nav_Exp/index.tsx
+++ src/component/Nav_Exp/index.tsx
@@ -15,81 +15,67 @@ import Cart from '../Cart';
 import HamburgerMenu from '../Hamburger_Menu';
 
 type Props = { cartData?: any; removeCartData?: any;} & React.HTMLAttributes<HTMLElement>;
-const Nav = ({ className, children,cartData, removeCartData, ...props }: Props) => {
-   
+
+interface NavProps {
+    className?: string;
+    children?: React.ReactNode;
+    cartData?: any;
+    removeCartData?: string;
+  }
+
+  const Nav: React.FC<NavProps> = ({ className, children, cartData, removeCartData }) => {  
     const [showDropdown, setShowDropdown] = useState(false);
-    const [cartOpen, setCartOpen] = useState<boolean>(false);
-    const adminStorage = localStorage.getItem('admin') && JSON.parse(localStorage.getItem('admin'));
-    const [admin, setAdmin] = useState(adminStorage ? adminStorage : undefined);
+    const [cartOpen, setCartOpen] = useState(false);
+    // const [admin, setAdmin] = useState();
+    // const [cartNumber, setCartNumber] = useState(0);
+    // const [cartItem, setCartItem] = useState([]);
+    const [admin, setAdmin] = useState<any>(undefined);
+const [cartItem, setCartItem] = useState<any[]>([]);
+const [cartNumber, setCartNumber] = useState<number>(0);
 
-    const cartStorage =localStorage.getItem('cart') && JSON.parse(localStorage.getItem('cart'));
-    const [cartNumber, setCartNumber] = useState(cartStorage?.length);
-    const [cartItem, setCartItem] = useState(cartStorage);
+  
+useEffect(() => {
+    const storedAdmin = localStorage.getItem("admin");
+    const parsedAdmin = storedAdmin ? JSON.parse(storedAdmin) : undefined;
+    setAdmin(parsedAdmin);
 
-    useEffect(() => {
-        console.log(props);
-        if (props.cartData) {
-          console.log(props);
-          if (cartNumber) {
-            // if (cartItem[0].resto_id != props.cartData.resto_id) {    //restaurant id same hoy to else vado run thase baki different hoy to new resto id nu food cart ma add thase
-            //   localStorage.removeItem('cart');
-            //   setCartNumber(1);
-            //   setCartItem([props.cartData])
-            //   localStorage.setItem('cart', JSON.stringify([props.cartData]))
-            // }
-            // else {
-              let localCartItem = cartItem;
-              localCartItem.push(JSON.parse(JSON.stringify(props.cartData)))    //shallow copy deep copy
-              setCartItem(localCartItem);
-              setCartNumber(cartNumber + 1)
-              localStorage.setItem('cart', JSON.stringify(localCartItem))
-            // }
-          }
-          else {
-            setCartNumber(1);
-            setCartItem([props.cartData])
-            localStorage.setItem('cart', JSON.stringify([props.cartData]))
-          }
-        }
-      }, [props.cartData])
+    const storedCart = localStorage.getItem("cart");
+    const parsedCart = storedCart ? JSON.parse(storedCart) : [];
+    setCartItem(parsedCart);
+    setCartNumber(parsedCart.length);
+  }, []);
 
+  useEffect(() => {
+    if (cartData) {
+      const newItem = JSON.parse(JSON.stringify(cartData));
+      const updatedCart = [...cartItem, newItem];
+      setCartItem(updatedCart);
+      setCartNumber(updatedCart.length);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+    }
+  }, [cartData]);
 
-      useEffect(() => {
-        if (props.removeCartData) {
-          let localCartItem = cartItem.filter((item) => {
-            return item._id != props.removeCartData
-          });
-          setCartItem(localCartItem);
-          setCartNumber(cartNumber-1);
-          localStorage.setItem('cart', JSON.stringify(localCartItem))
-          if(localCartItem.length==0){
-            localStorage.removeItem('cart')
-          }
-        }
-      }, [props.removeCartData])
+  useEffect(() => {
+    if (removeCartData) {
+      const updatedCart = cartItem.filter((item) => item._id !== removeCartData);
+      setCartItem(updatedCart);
+      setCartNumber(updatedCart.length);
+      if (updatedCart.length > 0) {
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
+      } else {
+        localStorage.removeItem("cart");
+      }
+    }
+  }, [removeCartData]);
 
-      useEffect(()=>{
-        if(props.removeCartData){
-          setCartItem([]);
-          setCartNumber(0);
-          localStorage.removeItem('cart')
-        }
-      },[props.removeCartData])
+  const handleCartClick = () => {
+    if (cartNumber > 0) setCartOpen(!cartOpen);
+  };
 
-    //   useEffect(()=>{
-    //     if(props.removeCartData){
-    //       setCartItem([]);
-    //       setCartNumber(0);
-    //       localStorage.removeItem('cart')
-    //     }
-    //   },[props.removeCartData])
+  const toggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
 
-    const handleCartClick = () => {
-        setCartOpen(!cartOpen); // Toggle the cart dialog visibility
-    };
-    const toggleDropdown = () => {
-        setShowDropdown(!showDropdown);
-    };
     return (
         <div className=''>
             <nav className='fixed md:block hidden'>
@@ -112,22 +98,6 @@ const Nav = ({ className, children,cartData, removeCartData, ...props }: Props) 
                                     <div className="ml-6 md:block hidden">
                                         <ul className="flex space-x-7 font-inter text-md font-medium lg:text-md xl:text-lg 2xl:text-xl">
                                             <li className=" font-sans text-black hover:text-red-500 cursor-pointer transform transition-transform duration-300  "><a href="/">Home</a></li>
-                                            {/* <li
-                                                className="relative group cursor-pointer transform transition-transform duration-300 "
-                                                onMouseEnter={toggleDropdown}
-                                                onMouseLeave={toggleDropdown}
-                                            >
-                                                <a href="/List" className="font-sans text-black hover:text-red-500">Accomodation</a>
-                                                {showDropdown && (
-                                                    <ul className="absolute top-5 left-0 mt-2 w-80 bg-white rounded-md shadow-lg py-2">
-                                                        
-                                                        <li><Link href="/List" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Newborn (0 m - 3 m)</Link></li>
-                                                        <li><Link href="/List" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Girls (3 m - 6 y)</Link></li>
-                                                        <li><Link href="/List" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Boys (3 m - 6 y)</Link></li>
-                                                        
-                                                    </ul>
-                                                )}
-                                            </li> */}
                                             <li><Link href="/List" className="font-sans text-black hover:text-red-500 cursor-pointer transform transition-transform duration-300 ">Accomodation</Link></li>
                                             <li><Link href="/Evnt" className="font-sans text-black hover:text-red-500 cursor-pointer transform transition-transform duration-300 ">Events</Link></li>                                            
                                             <li><Link href="/About" className="font-sans text-black hover:text-red-500 cursor-pointer transform transition-transform duration-300 ">About</Link></li>
@@ -167,7 +137,7 @@ const Nav = ({ className, children,cartData, removeCartData, ...props }: Props) 
                                             </ul>
                                         )}
                                     </div>
-                                    <div className="h-10 w-10 inline-flex items-center ml-3 text-black hover:text-blue-600 cursor-pointer relative" onClick={cartNumber > 0 ? handleCartClick : null}><IoCartOutline />
+                                    <div className="h-10 w-10 inline-flex items-center ml-3 text-black hover:text-blue-600 cursor-pointer relative" onClick={cartNumber > 0 ? handleCartClick : undefined}><IoCartOutline />
                                         <span className="absolute top-0 right-0 h-5 w-5 bg-red-500 text-white text-sm rounded-full flex items-center justify-center">
                                         {cartNumber ? cartNumber : 0}
                                         </span>
